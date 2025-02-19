a93855b554c8f8a8c6a354bd8fa7ba71285f37ea
theobeo2004 <thenhuet2004@gmail.com>
2025-02-19 10:05:53 +0700

    Project 2: feat - create new listing

--- Project 2/auctions/views.py
+++ Project 2/auctions/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from .models import User
+from .forms import ListingForm
 
 
 def index(request):
@@ -61,3 +63,24 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required  
+def create_listing(request):
+    
+    if (request.method == "POST"):
+        form = ListingForm(request.POST)
+        
+        if (form.is_valid()):
+            listing = form.save(commit=False)
+            listing.owner = request.user
+            listing.save()
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, "auctions/create_listing.html", {
+            "form": form
+        })
+            
+    else:
+        return render(request, "auctions/create_listing.html", {
+            "form": ListingForm()
+        })
